061bce1697b32c929dd5584f880ae4faac0b2e04
kevinptx <kevin.peery@gmail.com>
2018-07-19 09:05:59 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
-require 'pry'
+require "pry"
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentence = self.split(/\.|\?|\!/).delete_if do |w|
+      w.size < 2
+    end
+    split_sentence.size
   end
 end
